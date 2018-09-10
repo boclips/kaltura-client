4910c15b0d382f69a413039b853f31bb235fc4d3
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2018-09-10 23:08:12 +0100

    Add end-to-end test

--- src/test_contract/groovy/com/boclips/kalturaclient/KalturaClientE2E.groovy
+++ src/test_contract/groovy/com/boclips/kalturaclient/KalturaClientE2E.groovy
@@ -1,5 +1,6 @@
 package com.boclips.kalturaclient
 
+
 import spock.lang.Specification
 
 class KalturaClientE2E extends Specification {
@@ -7,17 +8,20 @@ class KalturaClientE2E extends Specification {
     def "fetch media entries from api"() {
         given:
         KalturaClientConfig config = KalturaClientConfig.builder()
-                .partnerId("x")
-                .userId("user@somewhere.com")
-                .secret("some secret")
+                .partnerId(System.getenv("PARTNER_ID"))
+                .userId(System.getenv("USER_ID"))
+                .secret(System.getenv("KALTURA_SECRET"))
                 .build()
 
         when:
         KalturaClient kalturaClient = KalturaClient.create(config)
-        List<MediaEntry> mediaEntries = kalturaClient.mediaEntriesByReferenceIds("239")
+        List<MediaEntry> mediaEntries = kalturaClient.mediaEntriesByReferenceIds(
+                "97eea646-c35b-4921-991d-95352666bd3a",
+                "750af1ea-cbeb-4047-8d48-7ef067bfedfb")
 
         then:
-        mediaEntries[0].id == 'x'
+        mediaEntries[0].id == '1_2t65w8sx'
+        mediaEntries[1].id == '1_8atxygq9'
     }
 
 }
